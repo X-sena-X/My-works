5c97a6ee7c777680cba72983d0dd9e7cf3829883
X-sena-X <101617549+X-sena-X@users.noreply.github.com>
2022-07-07 22:03:49 +0530

    Add files via upload

---
+++ React/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/navbar.js';
+import Intropage from './components/content-1';
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <div>
+        <Intropage/>
+      </div>
+    </>
+  );
+}
+
+export default App;
